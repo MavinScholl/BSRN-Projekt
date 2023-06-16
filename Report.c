0b58fcb24235e4e6c9fbbb2d3f4dc850607dfeca
Davide Pedergnana <130984608+Davipede99@users.noreply.github.com>
2023-06-16 14:16:19 +0200

    Create Report.c

---
+++ Report.c
@@ -0,0 +1,13 @@
+int main() {
+    int mittelwert, min, max, summe;
+
+    
+
+    // Ausgabe der Ergebnisse
+    printf("Der Mittelwert ist: %d\n", mittelwert);
+    printf("Der kleinste Wert ist: %d\n", min);
+    printf("Der größte Wert ist: %d\n", max);
+    printf("Die Summe ist: %d\n", summe);
+
+    return 0;
+}
